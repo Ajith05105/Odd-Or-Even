c6f207bb60fc3adda09e62f9fabc209cb62f577d
Ajith <ajith.penmatsa001@gmail.com>
2024-05-09 00:05:27 +1200

    verfied that the fingers are in between 0 and 5 and printed the amount of fingers and the playesrs name

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,18 +5,26 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  private String name;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
+     name = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
   public void play() {
 
     MessageCli.START_ROUND.printMessage("1");
+
     MessageCli.ASK_INPUT.printMessage();
-    MessageCli.INVALID_INPUT.printMessage();
-    
+    String fingers = Utils.scanner.nextLine();
+
+    if (Integer.parseInt(fingers) < 0 || Integer.parseInt(fingers) > 5) {
+      MessageCli.INVALID_INPUT.printMessage();
+    } else {
+      MessageCli.PRINT_INFO_HAND.printMessage(name, fingers);
+    }
   }
 
   public void endGame() {}
