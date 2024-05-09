323c7ca480c536672dc09fc747136b6c1d21b43e
Ajith <ajith.penmatsa001@gmail.com>
2024-05-09 12:28:21 +1200

    i made round a field variable so that everytime a play method is initialised the round variable increases by 1 and i set it to 0 initially

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,16 +6,18 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
   private String name;
+  private int round = 0;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
-     name = options[0];
+    name = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
   public void play() {
+    round++;
 
-    MessageCli.START_ROUND.printMessage("1");
+    MessageCli.START_ROUND.printMessage(String.valueOf(round));
 
     MessageCli.ASK_INPUT.printMessage();
     String fingers = Utils.scanner.nextLine();
