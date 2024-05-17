b164f6740ca0e5161431db2e1dd568020b1fb4a2
Ajith <ajith.penmatsa001@gmail.com>
2024-05-17 19:29:00 +1200

    did the java doc for easy method

--- src/main/java/nz/ac/auckland/se281/Difficulty/Easy.java
+++ src/main/java/nz/ac/auckland/se281/Difficulty/Easy.java
@@ -1,12 +1,20 @@
 package nz.ac.auckland.se281.Difficulty;
 
+import nz.ac.auckland.se281.Main.Choice;
 import nz.ac.auckland.se281.Strategy.Random;
 import nz.ac.auckland.se281.Strategy.Strategy;
-import nz.ac.auckland.se281.Main.Choice;
 
 public class Easy extends GameLevel {
   private String winner; // Variable to store the winner of the round
 
+  /**
+   * Play the game with the given number of fingers, player name, choice, and round number.
+   *
+   * @param fingers The number of fingers the player is holding out
+   * @param name The name of the player
+   * @param choice The choice the player made
+   * @param round The round number
+   */
   @Override
   public void play(int fingers, String name, Choice choice, int round) {
     // Create a new Strategy instance with the player's name, number of fingers, and choice
